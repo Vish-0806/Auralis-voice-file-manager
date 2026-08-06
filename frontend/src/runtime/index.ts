1c498de68cf03e0b0fb58d27883c53970ad4ed7c
Vish0806 <vishalsnaik27@gmail.com>
2026-08-06 11:46:33 +0530

    Implement Frontend Runtime Foundation with Vitest unit tests

---
+++ frontend/src/runtime/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Frontend Runtime Foundation Barrel Export (Phase 16.1).
+ */
+
+export * from './models';
+export * from './exceptions';
+export * from './interfaces';
+export * from './frontend_provider';
+export * from './frontend_runtime';
+export * from './runtime';
